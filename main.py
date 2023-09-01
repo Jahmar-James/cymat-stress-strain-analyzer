6d0279c1a7cd56baaf3582184b85e8bcf38369ca
Jahmar James <Jahmar.d.james@ryerson.ca>
2023-09-01 19:33:02 -0400

    testing

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ def main():
 
     app_path = sys.path[0]
     print(f"app_path: {app_path}")
-    import data_layer.models.specimen
+    import data_layer.models
+
+
+def main_dir():
+    import sys
+
+    _path = r"C:\Users\JahmarJames\OneDrive - Cymat Corporation\Documents\Python Scripts\Stress_Strain_App"
+
+    if _path not in sys.path:
+        sys.path.append(_path)
+
 
 
 if __name__ == "__main__":
